70eccc354d5354a30963798e18f8587dcae1e7e4
Hemanth <theultimatetwopiece2@gmail.com>
2026-08-31 16:07:43 +0800

    Initial Commit

---
+++ lesson6/lesson6App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson6App.swift
+//  lesson6
+//
+//  Created by Kakarla Hemanth Reddy on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct lesson6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lesson6Tests/lesson6Tests.swift
@@ -0,0 +1,19 @@
+//
+//  lesson6Tests.swift
+//  lesson6Tests
+//
+//  Created by Kakarla Hemanth Reddy on 31/8/26.
+//
+
+import Testing
+@testable import lesson6
+
+struct lesson6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
